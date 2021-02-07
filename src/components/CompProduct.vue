cd7f559f7f2e0c00dd7cfe364bc0df3ea847a55a
Sarah Wenzel <sarahrabea.wenzel@student.hs-rm.de>
2021-02-07 19:20:02 +0100

    doppeltes onmounted raus

--- src/components/CompProduct.vue
+++ src/components/CompProduct.vue
@@ -89,13 +89,6 @@ export default defineComponent({
             return Array.from(colorlist.value.keys());
 
         })
-        // sobald Komponente initialisiert ist, update() zum Füllen der "liste" ausführen
-        onMounted(async () => {
-            queryObject.room.value = route.query.room;
-            queryObject.productType.value = route.query.productType;
-            queryObject.name.value = "none";
-            await update();
-        });
         
         const productlist = computed(() => {
 
@@ -187,7 +180,7 @@ export default defineComponent({
                     return searchproductList.value;
                 }
             }), 
-            // productlist,
+            
             openProduct,
             closeFilter, 
             searchproductList,
