53a2fa8f31945a2f0edd30eb6471398a48932067
Isabelle Schmidt <isabelle.schmidt@student.hs-rm.de>
2021-02-11 17:09:32 +0100

    #240 11 weitere tests hinzugefügt (Componenten)

--- tests/unit/components/CompProduct.spec.ts
+++ tests/unit/components/CompProduct.spec.ts
@@ -8,20 +8,5 @@ describe('CompProduct.vue test', () => {
 
     test('if expected html structur', () => {
         expect(cmp.element).toMatchSnapshot()
-    })
-
-    // test('stubs a component using a custom component, component CartList is mentioned right in html', () => {
-
-    //     const ProductFilterMock = {	
-    //         name: 'ProductFilter',
-    //         template: 'fakeProductFilter'
-    //     }
-    //     const wrapper = mount(CompProduct, {
-    //         global: {
-    //         stubs: { CartList: ProductFilterMock }
-    //         }
-    //     }) 
-    //     expect(wrapper.html()).toContain(`<div class="productFilter"><fakeProductFilter/></div>`) 
-    // })
-      
+    }) 
 })
